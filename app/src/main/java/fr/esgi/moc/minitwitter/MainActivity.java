f7c629da6e8e4ef7c337701a11b34e53c124af55
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2018-05-08 16:03:23 +0200

    UI v2

--- app/src/main/java/fr/esgi/moc/minitwitter/MainActivity.java
+++ app/src/main/java/fr/esgi/moc/minitwitter/MainActivity.java
@@ -2,12 +2,33 @@ package fr.esgi.moc.minitwitter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.keywords_field) EditText keywordsField;
+    @BindView(R.id.recyclerview)   RecyclerView recyclerView;
+    @BindView(R.id.progressbar)    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.search_button) public void onClickSearch() {
+        progressBar.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.GONE);
+        // TODO call to presenter
     }
 }
