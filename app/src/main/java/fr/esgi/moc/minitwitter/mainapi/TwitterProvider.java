4ffd7299901b4c2700d371fabb6184ed26001a2b
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2018-05-10 16:02:25 +0200

    begin getToken()

--- app/src/main/java/fr/esgi/moc/minitwitter/mainapi/TwitterProvider.java
+++ app/src/main/java/fr/esgi/moc/minitwitter/mainapi/TwitterProvider.java
@@ -1,6 +1,12 @@
 package fr.esgi.moc.minitwitter.mainapi;
 
+import android.net.Uri;
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+
 import okhttp3.OkHttpClient;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,19 +15,36 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class TwitterProvider {
-    //private static final String BASE_URL = "https://swapi.co/api/";
+
+    private static final String BASE_URL = "https://api.twitter.com/";
     private static final String JSON_FORMAT = "json";
+    private static final String API_KEY = "6Hw3rFghxoQLVDBHjN183ovLI";
+    private static final String API_SECRET = "4V0hGqZVPc4Oge8eFDMGphjuWY8TPZ2CzBTlFQ4NhrgjkMhJlc";
 
     private TwitterService twitterService;
 
 
     public TwitterProvider() {
         Retrofit retrofit = new Retrofit.Builder()
-                //.baseUrl(BASE_URL)
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(createOkHttpClient())
                 .build();
         twitterService = retrofit.create(TwitterService.class);
+    }
+
+    public Call<String> getToken() throws UnsupportedEncodingException {
+        String bearerToken = createBearerToken();
+        byte[] bytesBearerToken = bearerToken.getBytes("UTF-8");
+        String base64BearerToken = Base64.encodeToString(bytesBearerToken, Base64.DEFAULT);
+
+        return twitterService.getToken(base64BearerToken);
+    }
+
+    private String createBearerToken() {
+        Uri uriParsedKey = Uri.parse(API_KEY);
+        Uri uriParsedSecret = Uri.parse(API_SECRET);
+        return uriParsedKey.toString() + ":" + uriParsedSecret.toString();
     }
 
     private OkHttpClient createOkHttpClient() {
